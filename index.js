75452ba50b9d2570a14df4d414662ad8f512b6e2
TheTrunk <tadeas.kmenta@zel.cash>
2020-04-07 16:59:53 +0800

    initial

---
+++ index.js
@@ -0,0 +1,258 @@
+const btcmessage = require('bitcoinjs-message');
+const qs = require('qs');
+const axios = require('axios');
+const zeltrezjs = require('zeltrezjs');
+const os = require('os');
+const fs = require('fs')
+
+const address = '';
+const privateKey = '';
+
+let logins = {};
+
+// return signature of a given message providing a private key
+async function signMessage(message, privKey) {
+  if (privKey.length !== 64) {
+    privKey = zeltrezjs.address.WIFToPrivKey(privKey);
+  }
+  const privateKey = Buffer.from(privKey, "hex");
+  const mysignature = btcmessage.sign(message, privateKey, true);
+  return mysignature.toString("base64");
+}
+
+function loadLogins() {
+  const platform = os.platform();
+  let loginsFile = null;
+
+  if (platform === "linux" || platform === "darwin") {
+    loginsFile = `${os.homedir()}/ZelFlux/logins.json`;
+  } else if (platform === "win32") {
+    loginsFile = `${os.homedir()}\\ZelFlux\\logins.json`;
+  }
+
+  if (fs.existsSync(loginsFile)) {
+    let data = "";
+    const stream = fs.createReadStream(loginsFile);
+    stream.on("data", (chunk) => {
+      data += chunk;
+    })
+      .on("end", () => {
+        logins = JSON.parse(data);
+      });
+  }
+}
+
+function saveLogins(logins) {
+  const platform = os.platform();
+  let loginsFile = null;
+
+  if (platform === "linux" || platform === "darwin") {
+    loginsFile = `${os.homedir()}/ZelFlux/logins.json`;
+  } else if (platform === "win32") {
+    loginsFile = `${os.homedir()}\\ZelFlux\\logins.json`;
+  }
+
+  const stream = fs.createWriteStream(loginsFile);
+
+  stream.once("open", () => {
+    stream.write(JSON.stringify(logins));
+    stream.end();
+  });
+}
+
+// given an ip address return stored authHeader
+async function getAuthHeader(ip) {
+  return logins[ip];
+}
+
+// returns login phrase of zelnode
+async function getLoginPhrase(ip) {
+  try {
+    const loginPhraseResponse = await axios.get(`http://${ip}:16127/zelid/loginphrase`);
+    if (loginPhraseResponse.data.status === 'success') {
+      return loginPhraseResponse.data.data;
+    } else {
+      throw loginPhraseResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// returns number of minor flux version
+async function getFluxVersion(ip) {
+  try {
+    const loginPhraseResponse = await axios.get(`http://${ip}:16127/zelflux/version`);
+    if (loginPhraseResponse.data.status === 'success') {
+      return Number(loginPhraseResponse.data.data.split('.')[1]);
+    } else {
+      throw loginPhraseResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// returns number of scannedheight
+async function getZelBenchVersion(ip) {
+  try {
+    const loginPhraseResponse = await axios.get(`http://${ip}:16127/zelbench/getinfo`);
+    if (loginPhraseResponse.data.status === 'success') {
+      return Number(loginPhraseResponse.data.data.version.split('.').join(''));
+    } else {
+      throw loginPhraseResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// returns number of scannedheight
+async function getScannedHeight(ip) {
+  try {
+    const loginPhraseResponse = await axios.get(`http://${ip}:16127/explorer/scannedheight`);
+    if (loginPhraseResponse.data.status === 'success') {
+      return loginPhraseResponse.data.data.generalScannedHeight;
+    } else {
+      throw loginPhraseResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function zelcashPing(ip) {
+  try {
+    authHeader = await getAuthHeader(ip);
+    console.log(authHeader);
+    const zelidauthHeader = authHeader;
+    console.log(zelidauthHeader);
+    const restartResponse = await axios.get(`http://${ip}:16127/zelcash/ping`, {
+      headers: {
+        zelidauth: zelidauthHeader,
+      },
+    });
+    if (restartResponse.data.status === 'success') {
+      console.log(ip + ': ' + restartResponse.data.data);
+    } else {
+      throw restartResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function restartNodeBenchmarks(ip) {
+  try {
+    authHeader = await getAuthHeader(ip);
+    const zelidauthHeader = authHeader;
+    const restartResponse = await axios.get(`http://${ip}:16127/zelbench/restartnodebenchmarks`, {
+      headers: {
+        zelidauth: zelidauthHeader,
+      },
+    });
+    if (restartResponse.data.status === 'success') {
+      console.log(ip + ': ' + restartResponse.data.data);
+    } else {
+      throw restartResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function restartDaemon(ip) {
+  try {
+    authHeader = await getAuthHeader(ip);
+    const zelidauthHeader = authHeader;
+    const restartResponse = await axios.get(`http://${ip}:16127/zelcash/restart`, {
+      headers: {
+        zelidauth: zelidauthHeader,
+      },
+    });
+    if (restartResponse.data.status === 'success') {
+      console.log(ip + ': ' + restartResponse.data.data);
+    } else {
+      throw restartResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateZelBench(ip) {
+  try {
+    authHeader = await getAuthHeader(ip);
+    const zelidauthHeader = authHeader;
+    const restartResponse = await axios.get(`http://${ip}:16127/zelnode/updatezelbench`, {
+      headers: {
+        zelidauth: zelidauthHeader,
+      },
+    });
+    if (restartResponse.data.status === 'success') {
+      console.log(ip + ': ' + restartResponse.data.data);
+    } else {
+      throw restartResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateZelFlux(ip) {
+  try {
+    authHeader = await getAuthHeader(ip);
+    const zelidauthHeader = authHeader;
+    const restartResponse = await axios.get(`http://${ip}:16127/zelnode/updatezelflux`, {
+      headers: {
+        zelidauth: zelidauthHeader,
+      },
+    });
+    if (restartResponse.data.status === 'success') {
+      console.log(ip + ': ' + restartResponse.data.data);
+    } else {
+      throw restartResponse.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// post login
+async function login(ip) {
+  try {
+    const loginPhrase = await getLoginPhrase(ip);
+    const signature = await signMessage(loginPhrase, privateKey);
+    const zelidauth = {
+      zelid: address,
+      signature,
+      loginPhrase,
+    };
+    const verifyLogin = await axios.post(`http://${ip}:16127/zelid/verifylogin`, qs.stringify(zelidauth));
+    if (verifyLogin.data.status === 'success') {
+      console.log(`Login to ${ip} success. Storing login data`);
+      const login = qs.stringify(zelidauth);
+      logins[ip] = login;
+      saveLogins(logins);
+      return true;
+    } else {
+      throw verifyLogin.data.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function massLogin() {
+  loadLogins();
+  setTimeout(() => {
+    console.log(logins);
+    updateZelFlux('62.171.163.150');
+  }, 2000);
+  // const loggedIn = await login('62.171.163.150');
+  // console.log(loggedIn)
+  // if true, you shall wait one second, then login another one
+}
+
+massLogin();
+
